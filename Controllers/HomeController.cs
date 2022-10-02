978a7d50cdd0262075ab62b5703526db1b0d3c67
Mahfuz <mahfuzkst2@gmail.com>
2022-10-03 02:19:41 +0600

    NewPost

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BloodBank.Models;
+
+namespace BloodBank.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BloodBankEntities db = new BloodBankEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult CustomHome()
+        {
+            var requisitions = db.Requisitions.Include(r => r.BloodGroup).Include(r => r.District).Include(r => r.Thana);
+            return View(requisitions.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/ThanasController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BloodBank.Models;
+
+namespace BloodBank.Controllers
+{
+    public class ThanasController : Controller
+    {
+        private BloodBankEntities db = new BloodBankEntities();
+
+        // GET: Thanas
+        public ActionResult Index()
+        {
+            var thanas = db.Thanas.Include(t => t.District);
+            return View(thanas.ToList());
+        }
+
+        // GET: Thanas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thana thana = db.Thanas.Find(id);
+            if (thana == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thana);
+        }
+
+        // GET: Thanas/Create
+        public ActionResult Create()
+        {
+            ViewBag.District_Id = new SelectList(db.Districts, "DistrictID", "DistrictName");
+            return View();
+        }
+
+        // POST: Thanas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ThanaID,District_Id,ThanaName,Status")] Thana thana)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Thanas.Add(thana);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.District_Id = new SelectList(db.Districts, "DistrictID", "DistrictName", thana.District_Id);
+            return View(thana);
+        }
+
+        // GET: Thanas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thana thana = db.Thanas.Find(id);
+            if (thana == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.District_Id = new SelectList(db.Districts, "DistrictID", "DistrictName", thana.District_Id);
+            return View(thana);
+        }
+
+        // POST: Thanas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ThanaID,District_Id,ThanaName,Status")] Thana thana)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thana).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.District_Id = new SelectList(db.Districts, "DistrictID", "DistrictName", thana.District_Id);
+            return View(thana);
+        }
+
+        // GET: Thanas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Thana thana = db.Thanas.Find(id);
+            if (thana == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thana);
+        }
+
+        // POST: Thanas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id) { 
+            Thana thana = db.Thanas.Find(id);
+            db.Thanas.Remove(thana);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
